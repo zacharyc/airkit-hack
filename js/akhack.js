050e6516bf4d921e56e2a27a32d07496a3ef1bd3
Zachary Cohen <zack@ruist.com>
2022-01-21 13:50:11 -0800

    fixing styles for the reveal button

--- js/akhack.js
+++ js/akhack.js
@@ -1,5 +1,6 @@
 window.onload = function() {
 	document.getElementById("reveal").addEventListener('click', () => {
-		document.getElementById("theme").style.display = "block"
+		document.getElementById("theme").style.display = "block";
+		document.getElementById("reveal").style.display = "none";
 	})
 }
